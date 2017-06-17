1fbf47f8c9817a400b404568df4b86162c3659bc
Lars K Oestergaard <l.ostergaard@elsevier.com>
2017-06-16 20:13:00 -0400

    added content types that are supported

---
+++ src/ContentType.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PureAPI
+{
+	/// <summary>
+	/// Available Pure API content types.
+	/// </summary>
+	public static class ContentType
+	{
+		/// <summary>
+		/// The activities.
+		/// </summary>
+		public static string Activities = "activities";
+		/// <summary>
+		/// The applications.
+		/// </summary>
+		public static string Applications = "applications";
+		/// <summary>
+		/// The awards.
+		/// </summary>
+		public static string Awards = "awards";
+		/// <summary>
+		/// The changes.
+		/// </summary>
+		public static string Changes = "changes";
+		/// <summary>
+		/// The classification schemes.
+		/// </summary>
+		public static string ClassificationSchemes = "classification-schemes";
+		/// <summary>
+		/// The concepts.
+		/// </summary>
+		public static string concepts = "concepts";
+		/// <summary>
+		/// The courses.
+		/// </summary>
+		public static string courses = "courses";
+		/// <summary>
+		/// The curricula vitae.
+		/// </summary>
+		public static string CurriculaVitae = "curricula-vitae";
+		/// <summary>
+		/// The datasets.
+		/// </summary>
+		public static string Datasets = "datasets";
+		/// <summary>
+		/// The equipments.
+		/// </summary>
+		public static string Equipments = "equipments";
+		/// <summary>
+		/// The events.
+		/// </summary>
+		public static string Events = "events";
+		/// <summary>
+		/// The external organisations.
+		/// </summary>
+		public static string ExternalOrganisations = "external-organisations";
+		/// <summary>
+		/// The external persons.
+		/// </summary>
+		public static string ExternalPersons = "external-persons";
+		/// <summary>
+		/// The fingerprints.
+		/// </summary>
+		public static string Fingerprints = "fingerprints";
+		/// <summary>
+		/// The impacts.
+		/// </summary>
+		public static string Impacts = "impacts";
+		/// <summary>
+		/// The journals.
+		/// </summary>
+		public static string Journals = "journals";
+		/// <summary>
+		/// The organisational units.
+		/// </summary>
+		public static string OrganisationalUnits = "organisational-units";
+		/// <summary>
+		/// The persons.
+		/// </summary>
+		public static string Persons = "persons";
+		/// <summary>
+		/// The press media.
+		/// </summary>
+		public static string PressMedia = "press-media";
+		/// <summary>
+		/// The prizes.
+		/// </summary>
+		public static string Prizes = "prizes";
+		/// <summary>
+		/// The projects.
+		/// </summary>
+		public static string Projects = "projects";
+		/// <summary>
+		/// The publishers.
+		/// </summary>
+		public static string Publishers = "publishers";
+		/// <summary>
+		/// The research outputs.
+		/// </summary>
+		public static string ResearchOutputs = "research-outputs";
+		/// <summary>
+		/// The semantic groups.
+		/// </summary>
+		public static string SemanticGroups = "semantic-groups";
+		/// <summary>
+		/// The thesauri.
+		/// </summary>
+		public static string Thesauri = "thesauri";
+	}
+
+}
